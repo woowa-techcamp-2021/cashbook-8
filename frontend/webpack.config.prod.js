aaf3955d5ac76ce33d856f70bacf143ff2b3ea68
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-07-26 17:50:12 +0900

    add: webpack 개발, 배포 설정

---
+++ frontend/webpack.config.prod.js
@@ -0,0 +1,48 @@
+require('dotenv/config');
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.js', '.json']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css?$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|jp(e*)g)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8000,
+          name: 'images/[hash]-[name].[ext]'
+        },
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.EnvironmentPlugin([]),
+  ],
+}
