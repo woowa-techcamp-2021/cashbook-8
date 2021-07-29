9e87cb8047e4872c5e34e37767f41615abcdd8b3
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-07-29 13:25:26 +0900

    add: Entity에 Payment와 CashHistory 추가

--- backend/src/entities/index.ts
+++ backend/src/entities/index.ts
@@ -1,9 +1,13 @@
+import CashHistory from './cash-history';
 import Category from './category';
+import Payment from './payment';
 import User from './user';
 
 const entities = [
   User,
-  Category
+  Category,
+  CashHistory,
+  Payment
 ];
 
 export default entities;
