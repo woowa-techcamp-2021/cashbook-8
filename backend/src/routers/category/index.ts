28a602317dd81650cf34031f781417a39ac5a7b6
chaeeun037 <chaeeun.dev@gmail.com>
2021-07-29 18:21:43 +0900

    add: category api router 추가

---
+++ backend/src/routers/category/index.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import categoryController from '../../controllers/category.controller';
+import wrapAsync from '../../lib/wrap-async';
+import authMiddleware from '../../middlewares/auth.middleware';
+
+const categoryRouter = Router();
+
+categoryRouter.get('/all', authMiddleware, wrapAsync(categoryController.findCategories));
+categoryRouter.post('', authMiddleware, wrapAsync(categoryController.createCategory));
+categoryRouter.delete('/:id', authMiddleware, wrapAsync(categoryController.deleteCategory));
+
+export default categoryRouter;
--- backend/src/routers/index.ts
+++ backend/src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import authRouter from './auth';
 import userRouter from './user';
+import categoryRouter from './category';
 
 const apiRouter = Router();
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/user', userRouter);
+apiRouter.use('/category', categoryRouter);
 
 export default apiRouter;
