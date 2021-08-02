f88225959a02e3bc85ca68eb60144f3f452cf5e7
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-02 17:11:46 +0900

    fix: Github OAuth URL 상수 분리

--- frontend/src/view-models/login.ts
+++ frontend/src/view-models/login.ts
@@ -4,10 +4,11 @@ import { buildURL } from '../utils/path';
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID as string;
 const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI as string;
 
+const GITHUB_OAUTH_URL = 'https://github.com/login/oauth/authorize';
+
 class LoginViewModel extends ViewModel {
   onGithubLoginClicked (): void {
-    // URL 상수 분리 필요
-    const url = buildURL('https://github.com/login/oauth/authorize', {
+    const url = buildURL(GITHUB_OAUTH_URL, {
       query: {
         client_id: GITHUB_CLIENT_ID,
         redirect_uri: GITHUB_REDIRECT_URI
