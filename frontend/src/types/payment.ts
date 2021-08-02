483370489cfbd26ef68ca1b3ccc431035b2fe292
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-02 15:37:13 +0900

    add: 결제수단 fetch 후 모델 등록

--- frontend/src/types/payment.ts
+++ frontend/src/types/payment.ts
@@ -1,5 +1,11 @@
+import { BaseResponse } from './base-response';
+
 export type Payment = {
   id: number;
   name: string;
   userId: number;
 }
+
+export type PaymentsResponse = {
+  payments: Payment[];
+} & BaseResponse;
