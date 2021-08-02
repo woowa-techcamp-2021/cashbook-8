365ea7bb0a3bb305144660ae60e58be93f025855
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-02 19:07:58 +0900

    add: CalendarCell UIElement 개발

---
+++ frontend/src/ui-elements/calendar-cell/index.ts
@@ -0,0 +1,38 @@
+import UIElement from '../../core/ui-element';
+import { CashHistoriesInDay } from '../../types/cash-history';
+
+import './index.css';
+
+class CalendarCell extends UIElement {
+  private cashHistoriesInDay: CashHistoriesInDay;
+
+  constructor ($target: HTMLElement, cashHistoriesInDay: CashHistoriesInDay) {
+    super($target);
+    this.cashHistoriesInDay = cashHistoriesInDay;
+  }
+
+  protected render (): void {
+    const { date, income, expenditure } = this.cashHistoriesInDay;
+    this.$element.innerHTML = `
+      <td class="calendar-cell">
+        <div class="calendar-cell__price-wrapper">
+          <span class="calendar-cell__price calendar-cell__price--income">${income}</span>
+          <span class="calendar-cell__price calendar-cell__price--expenditure">${expenditure}</span>
+          <span class="calendar-cell__price calendar-cell__price--total">${expenditure - income}</span>
+        </div>
+
+        <div class="calendar-cell__date">${date}</div>
+      </td>
+    `;
+  }
+
+  protected addListener (): void {
+    // no event
+  }
+
+  protected mount (): void {
+    // no mount
+  }
+}
+
+export default CalendarCell;
