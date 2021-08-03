839874aa773c30e4bfc990aeb911b6870aed9652
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-03 20:05:35 +0900

    add: yyyyMMdd 형식을 Date로 변경하는 유틸리티

--- backend/src/utils/date.ts
+++ backend/src/utils/date.ts
@@ -1,3 +1,17 @@
+import { isNumberString } from 'class-validator';
+
 export const getDaysInMonth = (year: number, month: number): number => {
   return new Date(year, month, 0).getDate();
+};
+
+export const yyyyMMdd2Date = (yyyyMMdd: string): Date => {
+  const year = yyyyMMdd.slice(0, 4);
+  const month = yyyyMMdd.slice(4, 6);
+  const date = yyyyMMdd.slice(6, 8);
+
+  if (!(isNumberString(year) && isNumberString(month) && isNumberString(date))) {
+    throw new Error(`${yyyyMMdd} 은 yyyyMMdd 양식에 맞지 않습니다`);
+  }
+
+  return new Date(Number(year), Number(month) - 1, Number(date));
 };
