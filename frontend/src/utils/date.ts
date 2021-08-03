126f427088e43a51f3611f4c9ecae95601444d73
chaeeun037 <chaeeun.dev@gmail.com>
2021-08-03 14:29:06 +0900

    fix: getDayString 매개변수 에러 처리

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -5,6 +5,9 @@ export const isSameDate = (dateA: Date, dateB: Date): boolean => {
 };
 
 export const getDayString = (day: number): string => {
+  if (day > 6 || day < 0) {
+    return '';
+  }
   const dayString = ['일', '월', '화', '수', '목', '금', '토', '일'];
   return dayString[day];
 };
