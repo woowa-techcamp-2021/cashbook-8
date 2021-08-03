490c3109736b599a3ae1e068112b0416961168dd
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-03 21:22:51 +0900

    add: 카테고리별 정렬된 지출을 조회 ViewModel

---
+++ frontend/src/view-models/expenditure-in-month.ts
@@ -0,0 +1,101 @@
+import cashHistoryAPI from '../api/cash-history';
+import categoryAPI from '../api/category';
+import actions from '../constant/actions';
+import pubsub from '../core/pubsub';
+import View from '../core/view';
+import ViewModel from '../core/view-model';
+import { CashHistories } from '../enums/cash-history.enum';
+import models from '../models';
+import { CashHistoriesData } from '../models/cash-histories';
+import { CategoriesData } from '../models/categories';
+import { FocusDateData } from '../models/focus-date';
+import { CashHistory } from '../types/cash-history';
+import { Category } from '../types/category';
+
+type ExpenditureGroupedByCategory = {
+  expenditure: number;
+  rate: number;
+  category: Category;
+}
+
+class ExpenditureInMonthViewModel extends ViewModel {
+  private cashHistoriesModel: CashHistoriesData;
+  private focusDateModel: FocusDateData;
+  private categoriesModel: CategoriesData;
+
+  constructor (view: View) {
+    super(view);
+    this.cashHistoriesModel = models.cashHistories;
+    this.focusDateModel = models.focusDate;
+    this.categoriesModel = models.categories;
+
+    this.fetchCashHistories();
+    this.fetchCategories();
+  }
+
+  private async fetchCategories () {
+    const categories = await categoryAPI.fetchCategories();
+    this.categoriesModel.categories = categories;
+  }
+
+  private async fetchCashHistories () {
+    const { focusDate } = this.focusDateModel;
+    const year = focusDate.getFullYear();
+    const month = focusDate.getMonth() + 1;
+
+    const cashHistories = await cashHistoryAPI.fetchCashHistories(year, month);
+    this.cashHistoriesModel.cashHistories = cashHistories;
+  }
+
+  protected subscribe (): void {
+    pubsub.subscribe(actions.ON_FOCUS_DATE_CHANGE, () => {
+      this.fetchCashHistories();
+    });
+
+    pubsub.subscribe(actions.ON_CASH_HISTORY_CHANGE, () => {
+      this.view.build();
+    });
+
+    pubsub.subscribe(actions.ON_CATEGORIES_CHANGE, () => {
+      this.view.build();
+    });
+  }
+
+  get expenditureGroupedByCategory (): ExpenditureGroupedByCategory[] {
+    if (this.categoriesModel.categories === null ||
+      this.cashHistoriesModel.cashHistories === null) {
+      return [];
+    }
+
+    const { cashHistories } = this.cashHistoriesModel.cashHistories;
+    const categories = this.categoriesModel.categories.categories.filter((category) => {
+      return category.type === CashHistories.Expenditure;
+    });
+
+    const { totalExpenditure, groupedCashHistories } = cashHistories;
+    const totalCashHistories = groupedCashHistories.reduce((totalCashHistories, dailyCashHistory) => {
+      return [
+        ...totalCashHistories,
+        ...dailyCashHistory.cashHistories
+      ];
+    }, [] as CashHistory[]);
+
+    const cashHistoriesGroupedByCategory = categories.map((category) => {
+      const expenditure = totalCashHistories
+        .filter((cashHistory) => cashHistory.categoryId === category.id)
+        .reduce((sum, cashHistory) => sum + cashHistory.price, 0);
+
+      return {
+        category,
+        expenditure,
+        rate: expenditure / totalExpenditure * 100
+      };
+    });
+
+    cashHistoriesGroupedByCategory.sort((a, b) => b.expenditure - a.expenditure);
+
+    return cashHistoriesGroupedByCategory;
+  }
+}
+
+export default ExpenditureInMonthViewModel;
