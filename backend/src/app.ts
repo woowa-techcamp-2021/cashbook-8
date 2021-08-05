43f1d80149f71bf0a8b2ef8f59144bf3d9f03659
chaeeun037 <chaeeun.dev@gmail.com>
2021-08-05 11:35:31 +0900

    add: 프론트엔드 빌드 정적 파일 서빙

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,6 +3,7 @@ import apiRouter from './routers';
 import cors from 'cors';
 import dotenv from './config/dotenv';
 import errorMiddleware from './middlewares/error.middleware';
+import * as path from 'path';
 
 const app = express();
 
@@ -11,6 +12,9 @@ app.use(cors({
 }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use('/dist', express.static(path.join(__dirname, '../../frontend/dist')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../frontend/dist/index.html')));
 
 app.use('/api', apiRouter);
 
